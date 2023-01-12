75654dd6bc22bf3cda809aa94d86c1e06e5d6ae6
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2023-01-12 12:29:52 +0100

    task: Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Returns a text to the client
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returns HBNB to the client
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    Return C is <text> to client
+    """
+    return "C {}".format(' '.join(text.split("_")))
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text="is_cool"):
+    """
+    Return Python <is cool> to client
+    """
+    return "Python {}".format(' '.join(text.split('_')))
+
+
+if __name__ == '__main__':
+    """
+    Runs when this script is not imported
+    """
+    app.run(host="0.0.0.0", port=5000)
